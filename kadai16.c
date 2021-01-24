f6b9ac77923383a7abc28a19e69caf9900a135ee
butsuli-shine <luluchey.1223@gmial.com>
2021-01-24 11:18:21 +0900

    add: コメントを追加

--- kadai16.c
+++ kadai16.c
@@ -1,25 +1,28 @@
+// coding-UTF-8
 // kadai16 台形公式による数値積分
 
 #include <stdio.h>
 #include <math.h>
 
-#define N 30
+#define N 30// 積分刻み数N
 
-#define f(x) (((x)*(x)*(x)*(x))+2*x)
+#define f(x) (((x)*(x)*(x)*(x)) + 2*x)// f(x)のdefine定義
 
 int main()
 {
-    double y[N+1] ;
-    double xa = 0.0, xb = 3.0;
+    double y[N+1];// f(x)の値を格納するdouble型配列
+    double xa = 0.0, xb = 3.0;// 積分区間の設定
     double z = 0.0, h = 0.0, x, s;
 
     h = (xb -xa) / (double)N;
 
+    // 数値積分
     for (int i = 0; i <= N; i++){
         x = xa + (h * (double) i);
         y[i] = f(x);
     }
 
+    // 結果の和
     for (int i = 1; i < N; i++){
         z += 2.0 * y[i];
     }
